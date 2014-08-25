53e6ed15949afa0da27e33de45570a2a8bcb6002
Benjamin A. Laken <benlaken@gmail.com>
2014-08-25 11:07:48 +0200

    added kojis read shape of lakes

--- Read_functions.py
+++ Read_functions.py
@@ -1,1 +1,15 @@
 # Start of function file 
+
+import numpy as np
+from osgeo import ogr
+
+fn = 'comsat.geojson'
+driver = ogr.GetDriverByName('GeoJSON')
+ds = driver.Open(fn)
+lyr = ds.GetLayer('OGRGeoJSON')
+dfn = lyr.GetLayerDefn()
+fieldnames = [dfn.GetFieldDefn(i).GetName() 
+              for i in range(dfn.GetFieldCount())]
+
+## use comsat.Altitude for the lake elevation for now... it will be different 
+## for the many other lakes. This is only a pracice set. 
