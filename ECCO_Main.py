10706c4f9854b67e9f7d140090eb5303beb40590
Benjamin Laken <blaken@geo.uio.no>
2014-10-30 17:12:35 +0100

    ready to script!

--- ECCO_Main.py
+++ ECCO_Main.py
@@ -3,7 +3,6 @@ from multiprocessing import Process,Pool,cpu_count
 import time
 import math
 
-start = time.time()
 
 nc_path = '/uio/kant/geo-metos-u1/blaken/Downloads/'+\
           'tas_EUR-11_ICHEC-EC-EARTH_rcp85_r3i1p1_DMI-'+\
@@ -11,34 +10,24 @@ nc_path = '/uio/kant/geo-metos-u1/blaken/Downloads/'+\
 lake_data = 'Lakes/largest100.geojson'
 out_path = 'Outputs'
 
-#lake_num = 5
-#MT_Means_Over_Lake(nc_path,lake_data,\
-#                4,out_path,plots=True,rprt_tme=True)
 
 
-
-# Nb. Below, i, an incrementing integer, represents the lake number
-# range(n) below is a simple way of hardcoding the total number of lakes
-# to run though the file.
-processes= [Process(target=MT_Means_Over_Lake, args=(nc_path,lake_data,\
+if __name__ == "__main__":
+    start = time.time()
+    processes= [Process(target=MT_Means_Over_Lake, args=(nc_path,lake_data,\
                     i,out_path,), kwargs={'plots':False,'rprt_tme':False,})\
-                                    for i in range(24)]
+                                    for i in range(24)] # Run x lakes 
 
-
-cpus = cpu_count() -1  # number of CPU's and leave some free...
-cpus = 6
-p_groups= int(math.ceil(float(len(processes))/float(cpus))) # how many groups 
-
-srng = 0
-frng = cpus
-for n in xrange(p_groups):
-    [p.start() for p in processes[srng:frng]]
-    [p.join() for p in processes[srng:frng]]
-    #print 'loop',n,' of ', p_groups,'  group:',srng,':',frng
-    srng+= cpus
-    frng+= cpus  # increment start and finish by the number of CPUs
-    Update_Progress(float(n)/(float(p_groups)-1.))  # A small progress bar...
-
-
-fin_time = time.time()
-print '\n','Finished processing in %6.2f min.'%((fin_time-start)/60.)
+    cpus = cpu_count() -2  # CPU's to work on (leave some free...)?
+    p_groups= int(math.ceil(float(len(processes))/float(cpus))) # How many groups?
+    srng = 0
+    frng = cpus
+    for n in xrange(p_groups):
+        [p.start() for p in processes[srng:frng]]
+        [p.join() for p in processes[srng:frng]]
+        #print 'loop',n,' of ', p_groups,'  group:',srng,':',frng
+        srng+= cpus
+        frng+= cpus  # increment start and finish by the number of CPUs
+        Update_Progress(float(n)/(float(p_groups)-1.))  # A small progress bar...
+    fin_time = time.time()
+    print '\n','Finished processing in %6.2f min.'%((fin_time-start)/60.)
