61d50046826d152c7f404df4d408b343437fe047
Benjamin Laken <blaken@geo.uio.no>
2014-11-11 16:24:28 +0100

    getting close

--- ECCO_Main.py
+++ ECCO_Main.py
@@ -5,30 +5,30 @@
 
 # NB. this should be run as: nice ipython ECCO_Main.py
 
-from ECCO_functions import *
+from ECCO_functions_v2 import *
 from multiprocessing import Process,Pool,cpu_count
 import time
 import math
 import glob
 
+lake_data = '/uio/kant/geo-metos-u1/blaken/datadisk/ECCO/Lakes/ecco-biwa_lakes_v.0.1.shp'
+#lake_data = 'Lakes/largest100.geojson'
+out_path = '/uio/kant/geo-metos-u1/blaken/datadisk/ECCO/Outputs'
 
-lake_data = 'Lakes/largest100.geojson'
-out_path = 'Outputs'
-
-
+# Search for all files in the below location with *.nc and generate a list.
 nc_folder = '/uio/kant/geo-metos-u1/blaken/datadisk/ECCO/CORDEX/'
-nc_list = glob.glob(nc_folder+'*.nc')
+nc_list = glob.glob(nc_folder+'*.nc')  
 
 
-'''
+
 if __name__ == "__main__":
     start = time.time()
 
     for nfile in nc_list:   # For each netcdf file in a list to processes
-        print 'Processing:', nfile
+        print 'Running on:', nfile[50:]
         processes= [Process(target=MT_Means_Over_Lake, args=(nfile,lake_data,\
                     i,out_path,), kwargs={'plots':False,'rprt_tme':False,})\
-                                    for i in range(30)] # Run x lakes 
+                                    for i in range(10)] # Run x lakes 
 
         cpus = cpu_count() -2  # CPU's to work on (leave some free)
         p_groups= int(math.ceil(float(len(processes))/float(cpus))) # Num groups
@@ -41,7 +41,6 @@ if __name__ == "__main__":
             srng+= cpus
             frng+= cpus  # increment start and finish by the number of CPUs
             Update_Progress(float(n)/(float(p_groups)-1.))  # A small progress bar...
-        fin_time = time.time()
-        print '\n','Finished processing in %6.2f min.'%((fin_time-start)/60.)
+    fin_time = time.time()
+    print '\n','Finished processing in %6.2f min.'%((fin_time-start)/60.)
 
-'''
